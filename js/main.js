e8af14db9a05b4d3601caf90ef4e3f83665b31e2
John Favors <JohnnyFavorite@Johns-MacBook-Pro.local>
2017-02-07 00:24:41 -0600

    first submit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+//ROUND 1 length of array strings
+var someArray = ["hello", "what", "is", "up", "dude"]
+function lengths(arrayOfStrings){
+	var lengthOfWords = [];
+	for (i = 0; i <someArray.length; i++) {
+		return(someArray[i].length)
+			lengthOfWords.push(someArray[i].length)
+				console.log(lengthOfWords)
+	}
+}  
+lengths(someArray)
+
+
+//ROUND 2 transmongrifier
+var something = {transmongrifier: 5, 4, 3; transmongrifier: 13, 12, 5; transmongrifier: 42, 13, 7}
+function transmongrifier(x,y,z) {
+	//my logic for each set of transmongrifier values perform the following loop
+for (i =0; i >=transmongrifier.length; i++)
+	return Math.pow(x * y, z)
+}
+
+
+
+//ROUND 3 Toonify
+var sentence = [accent: daffy, sentence:"so you smell like sausage", accent: elmer, sentence: "that's one tricky rabbit"]
+function toonify(accent, sentence) {
+	if accent === daffy
+		for (i = 0; i <= sentence.length; i++)  //iterate thrugh each character	//change all string value s to th			else if accent = elmer
+			cstr.replace(regexp|s, "th")
+				return(sentence)
+	}else if accent === elmer {
+			cstr.replace(regext|r, "w")
+			return(sentence)
+	}else {		
+			return(sentence)	
+}
+
+//ROUND 4  Word Reverse
+
+var wordArray ["Now I know what a TV dinner feels like"];
+function wordReverse(aString) {
+	for (i = 0; <= wordArray.length; i++)
+		return  wordArray.split('', wordArray.length);
+			wordArray.reverse
+				return(wordArray)
+					console.log(wordArray[i] + " ")
+
+}
+
+//ROUND 5 Letter Reverse
+var wordString ["Now I know what a TV dinner feels like"];
+function letterReverse(aStringAlso) {
+	for (i = 0; <=wordString.length; i++)
+		return wordString.split(wordstring.length);
+			wordstring.reverse
+				return(wordArray)
+					console.log(wordArray[i])
+}
+//ROUND 6 Longest
+var longArray = [longest(["oh", "good", "grief"]) # => "grief"
+longest(["Nothing" , "takes", "the", "taste", "out", "of", "peanut", "butter", "quite", "like", "unrequited", "love"])
+# => "unrequited"];
+function longest(arrayOfStrings) {
+	for (i = 0; i < longArray.length; i++)
+		return(longArray[i].length)
+			}if longArray[i].length  > longArray[i]+1{
+				return([i] + " is the longest") 
+					}else 
+						return [i]
+			}
+}
+
+
+//FINAL ROUND RepMaster
+
+var repMaster = [("Never give your heart to a blockhead", wordReverse) # =>
+"blockhead a to heart your give never proves that I am the rep MASTER!"
+repMaster("I finished this practice", toUpperCase);
+"I FINISHED THIS PRACTICE proves that I am the rep MASTER!"]
+function repMaster(input, function) {
+return( + "Proves" + " that" + " I" + " am" + " the" + "Rep" + "Master")
+}
